082db86832402c6c2136b6b91dc8c7c52079ef62
hjkim <fortressll87@gmail.com>
2018-10-04 13:26:30 +0900

    Add scp, hash source

---
+++ scp/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import io
+import os
+from setuptools import setup
+
+
+# Need to specify encoding for PY3
+with io.open('README.rst', encoding='utf-8') as fp:
+    description = fp.read()
+setup(
+        name = 'scp',
+        version = '0.11.0',
+        author = 'James Bardin',
+        author_email = 'j.bardin@gmail.com',
+        license = 'LGPL',
+        url = 'https://github.com/jbardin/scp.py',
+        description='scp module for paramiko',
+        long_description=description,
+        py_modules = ['scp'],
+        install_requires = ['paramiko'],
+        keywords=['paramiko', 'ssh', 'scp', 'transfer'],
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Internet',
+        ],
+)
